433d00d5f5e1ac1663943022adeb5e3532104b4c
Ash Hoover <strixcuriosus@gmail.com>
2014-06-11 21:17:26 -0700

    Removes unnecessary redirect to index in staticAssetsRouter.js

--- server/staticAssetsRouter.js
+++ server/staticAssetsRouter.js
@@ -1,9 +1,4 @@
 module.exports = exports = function(router) {
-  router.route('/')
-  .get(function(req, res){
-    res.redirect('/index.html');
-  });
-
   router.route('/emberTest')
   .get(function(req, res){
     res.redirect('/emberTest.html');
