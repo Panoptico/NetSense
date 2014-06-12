989bb76c8c888e69300033c2bde7e018f178225d
Ash Hoover <strixcuriosus@gmail.com>
2014-06-11 21:18:56 -0700

    Tests get requests to existing routes

--- specs/serverSpec.js
+++ specs/serverSpec.js
@@ -1,3 +1,5 @@
+//To run these tests from the root directory: mocha -R nyan specs/serverSpec.js
+
 var express = require('express');
 var supertest = require('supertest');
 var expect = require('chai').expect;
@@ -5,15 +7,28 @@ var url = require('url');
 
 var app = require('../server/app.js');
 
-describe('Server', function(){
-
-  beforeEach(function(){
-    console.log('\n');
-  });
-
-  it('should return 200 for get "/"  requests',function(done){
+describe('Server responses to get requests', function(){
+  it('Should return 200 for get "/"  requests', function(done){
     supertest(app)
       .get('/')
-      .expect(200, done);
+      .expect(200)
+      .expect('Content-Type', /html/);
+      done();
+  });
+
+  it('Should return 302 redirect for get "/static/v1/emberTest"', function(done){
+    supertest(app)
+      .get('/static/v1/emberTest')
+      .expect(302)
+      .end(function(err, res){
+          if (err) return done(err);
+          done();
+        });
+  });
+
+  it('Should return 404 for an invalid route', function(done){
+    supertest(app)
+      .get('/thisroutedoesnotexist')
+      .expect(404, done);
   });
 });
