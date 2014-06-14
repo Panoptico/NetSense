4edd07626507c7ec829d5fe85f9ce16f7ecd10e7
Ash Hoover <strixcuriosus@gmail.com>
2014-06-14 12:16:01 -0700

    Makes minor stylistic edits to tweetDataRouter.js

--- server/routers/tweetDataRouter.js
+++ server/routers/tweetDataRouter.js
@@ -3,27 +3,25 @@
 // e.g. localhost:8080/tweetdata/v1/tweet/477685207185645569
 
 var dbMethods = require('../../db/databaseHelpers');
+
 module.exports = exports = function(router) {
   router.route('/tweet/:tweetId')
-  .get(function(req, res){
-    console.log(req.params);
-    var tweetId = req.params.tweetId;
-    dbMethods.findTweetById('' + tweetId, function(err, data){
-      console.log(data);
-      res.send(data);      
+    .get(function(req, res) {
+      var tweetId = req.params.tweetId;
+      dbMethods.findTweetById('' + tweetId, function(err, data) {
+        res.send(data);      
+      });
     });
-  });
 
   router.route('/middle')
-  .get(
-    function(req, res, next) {
-      dbMethods.saveTweet({tweetId: 'tweet'}, next);
-    }, 
-    function(data, next) {
-      dbMethods.findTweetById(data.tweetId, next);
-    },
-    function(data) {
-      console.log(data);
-  });
-
+    .get(
+      function(req, res, next) {
+        dbMethods.saveTweet({tweetId: 'tweet'}, next);
+      }, 
+      function(data, next) {
+        dbMethods.findTweetById(data.tweetId, next);
+      },
+      function(data) {
+        console.log(data);
+    });
 };
