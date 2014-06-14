0ac9556b2356d39a2ff8ad3e2e0e82d027754367
Ash Hoover <strixcuriosus@gmail.com>
2014-06-14 12:09:49 -0700

    Implements a RESTful api for tweet data

--- server/routers/tweetDataRouter.js
+++ server/routers/tweetDataRouter.js
@@ -1,16 +1,29 @@
-// var url = require('url');
-// tweetid: 477685207185645569
+// to grab a tweet by it's id:
+// localhost:8080/tweetdata/v1/tweet/<tweetId>
+// e.g. localhost:8080/tweetdata/v1/tweet/477685207185645569
+
 var dbMethods = require('../../db/databaseHelpers');
 module.exports = exports = function(router) {
-  router.route('/users/*')
+  router.route('/tweet/:tweetId')
   .get(function(req, res){
-    // console.log(req.query);
-    var tweetId = '477685207185645569'; //req.query.tweetId;
+    console.log(req.params);
+    var tweetId = req.params.tweetId;
     dbMethods.findTweetById('' + tweetId, function(err, data){
       console.log(data);
       res.send(data);      
     });
   });
 
+  router.route('/middle')
+  .get(
+    function(req, res, next) {
+      dbMethods.saveTweet({tweetId: 'tweet'}, next);
+    }, 
+    function(data, next) {
+      dbMethods.findTweetById(data.tweetId, next);
+    },
+    function(data) {
+      console.log(data);
+  });
+
 };
-  
