d488542ff20039bcfdaf3ffe74f19e739de73f74
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-14 15:41:43 -0700

    Enables settings button to display SettingsView

--- client/www/js/router.js
+++ client/www/js/router.js
@@ -1,12 +1,21 @@
 NetSense.Router.map(function(){
   this.resource('NetSense', {path: '/'}, function(){
     this.route('dashboard');
+    this.route('settings');
   });
 });
 
 NetSense.NetSenseDashboardRoute = Ember.Route.extend({
   renderTemplate: function(controller){
     this.render('NetSense/dashboard', {
+      controller: controller
+    });
+  }
+});
+
+NetSense.NetSenseSettingsRoute = Ember.Route.extend({
+  renderTemplate: function(controller){
+    this.render('NetSense/settings', {
       controller: controller
     });
   }
