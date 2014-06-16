80320e26887dd1b5c81f83b81a9049a400c116a5
Andrew Krause <a.c.krause@gmail.com>
2014-06-16 12:03:30 -0700

    Adds user model to client

---
+++ client/www/js/models/user.js
@@ -0,0 +1,7 @@
+NetSense.User = DS.Model.extend({
+ twitterUserId: DS.attr('string'),
+ name: DS.attr('string'),
+ email: DS.attr('string'),
+ privateTracks: DS.hasMany('track'),
+ companyName: DS.attr('string')
+});
