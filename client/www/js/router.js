811e9d0460719d2849e133bb9cbf9d7641381b81
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-16 17:16:30 -0700

    Creates InformationView with two tabs

--- client/www/js/router.js
+++ client/www/js/router.js
@@ -1,6 +1,13 @@
 NetSense.Router.map(function(){
   this.resource('NetSense', {path: '/'}, function(){
+
+    this.resource('dashboard', {path: '/dashboard'}, function(){
+      this.route('tweetMap');
+      this.route('sentimentGraph');
+    });
+
     this.route('dashboard');
+
     this.route('settings');
     this.route('login');
   });
@@ -21,6 +28,26 @@ NetSense.NetSenseDashboardRoute = Ember.Route.extend({
     this.render('NetSense/dashboard', {
       outlet: 'ContentView',
       into: 'NetSense',
+      controller: controller
+    });
+  }
+});
+
+NetSense.DashboardTweetMapRoute = Ember.Route.extend({
+  renderTemplate: function(controller){
+    this.render('NetSense/dashboard/tweetMap', {
+      outlet: 'InformationView',
+      into: 'NetSense/dashboard',
+      controller: controller
+    });
+  }
+});
+
+NetSense.DashboardSentimentGraphRoute = Ember.Route.extend({
+  renderTemplate: function(controller){
+    this.render('NetSense/dashboard/sentimentGraph', {
+      outlet: 'InformationView',
+      into: 'NetSense/dashboard',
       controller: controller
     });
   }
