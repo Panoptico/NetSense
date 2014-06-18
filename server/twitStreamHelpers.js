dad94c588a06749ce3296678e1df8b7d271fd9ba
Nick Wei <weijin84@gmail.com>
2014-06-18 13:14:40 -0700

    Adds helper methods for tweeting and retweeting on behalf of an user

--- server/twitStreamHelpers.js
+++ server/twitStreamHelpers.js
@@ -1,4 +1,5 @@
-var Twit = require('Twit');
+var Twit = require('Twit'),
+    databaseHelpers = require('../db/databaseHelpers.js');
 
 var saveTweets = function(stream) {
   stream.on('tweet', function (tweet) {
@@ -19,5 +20,39 @@ module.exports = exports = {
     var stream = T.stream('statuses/filter', {track: track});
     saveTweets(stream);
     return stream;
+  },
+
+  sendRetweet: function(tweetId, token, tokenSecret) {
+    var T = new Twit({
+      consumer_key: process.env.CONSUMERKEY,
+      consumer_secret: process.env.CONSUMERSECRET,
+      access_token: token,
+      access_token_secret: tokenSecret
+    });
+
+    T.post('statuses/retweet/' + tweetId, {id: tweetId}, function(err, data, response) {
+      if (err) {
+        console.log('error:', err);
+      } else {
+        console.log('Retweeted!');
+      }
+    });
+  },
+
+  sendTweet: function(text, token, tokenSecret) {
+    var T = new Twit({
+      consumer_key: process.env.CONSUMERKEY,
+      consumer_secret: process.env.CONSUMERSECRET,
+      access_token: token,
+      access_token_secret: tokenSecret
+    });
+
+    T.post('statuses/update', {status: text}, function(err, data, response) {
+      if (err) {
+        console.log('error:', err);
+      } else {
+        console.log("Tweeted!");
+      }
+    });
   }
 };
