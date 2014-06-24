d0ad3148d6d377be01e56257617d5279bc18d3f8
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-24 15:31:10 -0700

    Prevents duplicate streams from starting

--- server/twitter/stream_controllers.js
+++ server/twitter/stream_controllers.js
@@ -2,7 +2,7 @@ var Twit = require('twit');
 var dbMethods = require('../../db/database_controllers.js');
 var tweetMethods = require('./tweet_controllers.js');
 
-var onTweet = function(tweet){
+var onTweet = function(tweet, trackName){
   var processedTweet = tweetMethods.processTweet(tweet);
   console.log('tweet processed!');
   dbMethods.saveTweet(processedTweet, function(err, data){
@@ -19,13 +19,24 @@ var onTweet = function(tweet){
   });
 }
 
-var saveTweetsToTrack = function(stream, trackName) {
+var saveTweetsToTrack = function(trackName, token, secret) {
   dbMethods.saveNewTrackByName(trackName, function(err, data) {
     if(err) {
       console.log('error1 track already exists');
       // return false to indicate stream already existed (and did not need to be started)
-      return false;
+      return;
     };
+
+    var T = new Twit({
+      consumer_key: process.env.TWITTER_CONSUMERKEY,
+      consumer_secret: process.env.TWITTER_CONSUMERSECRET,
+      access_token: token,
+      access_token_secret: secret
+    });
+
+    var stream = T.stream('statuses/filter', {track: trackName});
+    console.log('Created stream instance:', trackName);
+    
     stream.on('tweet', function (tweet) {
       console.log('tweet found!');
       onTweet(tweet, trackName);
@@ -37,20 +48,18 @@ var saveTweetsToTrack = function(stream, trackName) {
 
 module.exports = exports = {
   makeNewStream: function(track, token, secret) {
-    var T = new Twit({
-      consumer_key: process.env.TWITTER_CONSUMERKEY,
-      consumer_secret: process.env.TWITTER_CONSUMERSECRET,
-      access_token: token,
-      access_token_secret: secret
-    });
-    var stream = T.stream('statuses/filter', {'track': track});
-    console.log('created stream instance:', track);
+    saveTweetsToTrack(track, token, secret);
 
-    if(saveTweetsToTrack(stream, track)){
-      return stream;
-    } else {
-      delete stream;
-    }
+    // var T = new Twit({
+    //   consumer_key: process.env.TWITTER_CONSUMERKEY,
+    //   consumer_secret: process.env.TWITTER_CONSUMERSECRET,
+    //   access_token: token,
+    //   access_token_secret: secret
+    // });
+    // var stream = T.stream('statuses/filter', {'track': track});
+    // console.log('created stream instance:', track);
+
+    // need to find a different way of flagging true/false
   },
 
   sendRetweet: function(tweetId, token, tokenSecret) {
