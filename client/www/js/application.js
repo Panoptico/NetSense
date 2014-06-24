ca22c0383ff41eb575089ed72e5e71f11162efe6
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-24 16:44:31 -0700

    Adds HR Banner and login from index

--- client/www/js/application.js
+++ client/www/js/application.js
@@ -11,8 +11,7 @@ NetSense.ApplicationAdapter = DS.RESTAdapter.extend({
 
 DS.RESTAdapter.reopen({
   namespace: 'api/v1',
-  host: 'http://NetSenseDev.azurewebsites.net'
-  // host: 'http://localhost:8080'  
+  host: 'http://NetSense.azurewebsites.net' 
 });
 
 Ember.Inflector.inflector.uncountable('track');
