c1c19e85629e366ff0436f09c99a6f25ef8a973a
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-23 17:12:04 -0700

    Removes whitespace from user input tracks

--- client/www/js/controllers/DashboardController.js
+++ client/www/js/controllers/DashboardController.js
@@ -1,6 +1,7 @@
 NetSense.DashboardController = Ember.ObjectController.extend({
   actions:{
     onSubmit: function(trackName){
+      trackName = trackName.replace(/\s/g,'')
       var track = this.store.createRecord('track', {
         name: trackName
       });
