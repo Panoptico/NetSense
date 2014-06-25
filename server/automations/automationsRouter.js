5b4a12b26e93b6fb0712c6fb68c3dc928ada091a
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-25 10:54:50 -0700

    Fixes ringDoorbell automation route

--- server/automations/automationsRouter.js
+++ server/automations/automationsRouter.js
@@ -1,13 +1,14 @@
 var processor = require('../processing_controllers.js');
-var ringDoorbell = require('ringDoorbell.js')
+var ringDoorbell = require('./ringDoorbell.js')
+
 exports.route = function(tweet, trackName){
   nlp = processor.NLP(tweet);
 
   // default automation is ringDoorbell
   if(!nlp.intent){
-    nlp.intent = ringDoorbell;
+    nlp.intent = 'ringDoorbell';
   }
-
+  console.log(ringDoorbell)
   automations[nlp.intent](tweet, nlp, trackName);
 }
 
