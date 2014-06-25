cd4d05cf638efdf185fd4898ee018819ee0a4e93
Ash Hoover <strixcuriosus@gmail.com>
2014-06-24 23:36:27 -0700

    Sets up WIT.AI helper methods

---
+++ server/wit/wit_controllers.js
@@ -0,0 +1,22 @@
+var request = require('request');
+
+module.exports = exports = {
+  sendTextToWit: function(text) {
+    var headers = {
+      Authorization: 'Bearer ' + process.env.WIT_AI_KEY
+    };
+
+    var options = {
+      url: 'https://api.wit.ai/message',
+      headers: headers,
+      form: {q: text, v: '20140624'}
+    };
+
+    request.get(options, function(err, res, body){
+      if(err) return console.error(err);
+      body = JSON.parse(body);
+      var intent = body.outcome.intent;
+      console.log('outcome:', body);
+    });
+  }
+}
