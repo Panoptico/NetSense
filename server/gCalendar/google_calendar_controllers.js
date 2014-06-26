84b023d5238a1b9b03d522ebbcf952c0d182f0a9
Ash Hoover <strixcuriosus@gmail.com>
2014-06-26 09:18:00 -0700

    Includes methods for getting google oauth2 service account token and adding events to the NetSense google calendar

--- server/gCalendar/google_calendar_controllers.js
+++ server/gCalendar/google_calendar_controllers.js
@@ -1,46 +1,58 @@
-var http = require('http');
-var calendarOptions = {
-  host: 'www.google.com',
-  port: 80,
-  path: '/index.html'
-};
-
-http.get(options, function(res) {
-  console.log("Got response: " + res.statusCode);
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
-});
+var gcal = require('google-calendar');
+var request = require('request')
+var TokenCache = require('google-oauth-jwt').TokenCache;
+var tokens = new TokenCache();
 
 module.exports = exports = {
-  createNewEvent: function(calendar, event) {
+  createNetsenseEvent: function(event) {
+    //expect event to be an object with start, end, and summary properties
+    var body = JSON.stringify(event);
 
+    tokens.get({
+      // use the email address of the NetSense google service account, as seen in the API console
+      email: process.env.GOOGLE_SERVICE_EMAIL,
+      key: (process.env.GOOGLE_PRIV_KEY).replace(/\\n/g, '\n'),
+      // specify the scopes you wish to access
+      scopes: ['https://www.googleapis.com/auth/calendar']
+    }, function (err, token) {
+      var google_calendar = new gcal.GoogleCalendar(token);
+      var headers = {
+        Authorization: 'Bearer ' + token,
+        "Content-Type": 'application/json' 
+      };
+      var options = {
+        url: process.env.NETSENSE_CALENDAR_URL,
+        headers: headers,
+        key: process.env.GOOGLE_SERVICE_KEY,
+        body: body
+      };
+      request.post(options, function(err, res, body){
+              if(err) return console.error(err);
+              body = JSON.parse(body);
+              console.log(body);
+        });
+    });
   }
-
 };
 
-var serverKey = AIzaSyB1KaJ9DAecI3nOTXUw6xdcNx3ATO3GaSM;
-
-
-//TODO: set env variables for netsense calendar and key
-// 
+//event must have start, end, and summary properties
 
 /*
-  example event:
-  {
-  name: googleeventsummary,
-  start: datetime,
-  end: datetime,
-  organizer: optionalname,
-
-  }
-
+///////////
+//example of createNetsenseEvent:
 /////
 
-  example calendar:
+var newevent = {
+ end: {
+    date: "2014-06-27"
+   },
+ start: {
+    date: "2014-06-26"
+   },
+ summary: "this is a test"
+};
 
-  {
-    id: googlecalname@gmail.com,
-    key: googleapikeystring
-  
-  }
+exports.createNetsenseEvent(newevent);
+//creates a new event on the netsense calendar
+
 */
