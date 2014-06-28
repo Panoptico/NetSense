886be344ac0f2463c713f8e74f5f8f0e629bab8a
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-28 13:54:26 -0700

    Implements cheerio and request to get first link from google

--- server/automations/getInformation.js
+++ server/automations/getInformation.js
@@ -1,9 +1,32 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
 var getInformation = function(tweet, nlp, trackName){
   console.log(nlp.entities.search_query[0].value)
-  // link to let me google that for you...
-  var link = 'http://lmgtfy.com/?q=' + nlp.entities.search_query[0].value.split(' ').join('+');
+  var query = nlp.entities.search_query[0].value.split(' ').join('+')
+  if(Math.random < .75){
+    // 75% chance: first link in google results
+    var target = 'http://www.google.com/search?q=' + query;
+    var link = crawlForLink(target);
+  } else {
+    // 25% chance: link to let me google that for you...
+    var link = 'http://lmgtfy.com/?q=' + query;
+  }
   
   // replyToTweet(tweet, link);
+}
+
+var crawlForLink = function(target){
+  request(target, function(err, response, body){
+    if(err) console.error(err);
+    if(response.statusCode === 200){
+      $ = cheerio.load(body);
+      var link = $('.r > a')[0].attribs.href;
+      link = link.slice( link.indexOf('http'), link.indexOf('&sa=') );
+      return link;
+    }
+  }
+  return target;
 }
 
 module.exports = getInformation;
